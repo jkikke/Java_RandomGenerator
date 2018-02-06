e0bfdf6af85faf7baa46056e55a036278dfe75b3
Jkikke <Jkikke@192.168.0.14>
2018-02-06 17:43:02 +0200

    Test

--- RandomGenerator_tyot/src/randomgenerator_tyot/RandomGenerator_tyot.java
+++ RandomGenerator_tyot/src/randomgenerator_tyot/RandomGenerator_tyot.java
@@ -15,7 +15,7 @@ public class RandomGenerator_tyot {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        System.out.println("Hello World");
     }
     
 }
